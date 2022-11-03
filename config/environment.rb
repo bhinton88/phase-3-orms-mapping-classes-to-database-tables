223490564068da7384719baa642b7d5fe6781e0b
Bobby Hinton <bob.g.hint@gmail.com>
2022-11-03 13:36:56 -0500

    commit tree 27c9354d13465099cc2d8a1f5b5a9fc9aecc23ef

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,7 @@ Bundler.require
 
 require_relative '../lib/song'
 
+# we are assigning DB to a hash that includes a symbol
+# we are then able to access this in order to 
+# allow us to create a new database and access it
 DB = { conn: SQLite3::Database.new("db/music.db") }
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,46 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :album, :id
 
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
+    @id = id
     @name = name
     @album = album
+  end
+
+  # here we are calling a CLASS method to CREATE the table if it does 
+  # not already exist 
+
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?,?)
+      SQL
+
+      # inserts the song into the database
+      DB[:conn].execute(sql, self.name, self.album)
+
+      # gets the songs ID from the database and saves it to the Ruby instance
+      self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+      #remember the last line always auto returns. So here we are returning the Ruby instance with the ID value assigned
+      self
+  end
+
+  def self.create(name:, album:)
+    song = Song.new(name: name, album: album)
+    song.save
   end
 
 end
